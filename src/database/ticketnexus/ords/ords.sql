3d0bfd84d75d3225c58cda344c92e8fe49db3f90
pgraczyk <pgraczyk.de@gmail.com>
2026-03-07 13:46:36 +0100

    [feat] Project export

---
+++ src/database/ticketnexus/ords/ords.sql
@@ -0,0 +1,60 @@
+
+--
+        
+DECLARE
+  l_roles     OWA.VC_ARR;
+  l_modules   OWA.VC_ARR;
+  l_patterns  OWA.VC_ARR;
+
+BEGIN
+  ORDS.ENABLE_SCHEMA(
+      p_enabled             => TRUE,
+      p_url_mapping_type    => 'BASE_PATH',
+      p_url_mapping_pattern => 'ticketnexus',
+      p_auto_rest_auth      => TRUE);
+
+  ORDS.CREATE_ROLE(
+      p_role_name=> 'oracle.dbtools.role.autorest.TICKETNEXUS');
+  ORDS.CREATE_ROLE(
+      p_role_name=> 'oracle.dbtools.role.autorest.any.TICKETNEXUS');
+  l_roles(1) := 'oracle.dbtools.autorest.any.schema';
+  l_roles(2) := 'oracle.dbtools.role.autorest.TICKETNEXUS';
+  l_patterns(1) := '/metadata-catalog/*';
+
+  ORDS.DEFINE_PRIVILEGE(
+      p_privilege_name => 'oracle.dbtools.autorest.privilege.TICKETNEXUS',
+      p_roles          => l_roles,
+      p_patterns       => l_patterns,
+      p_modules        => l_modules,
+      p_label          => 'TICKETNEXUS metadata-catalog access',
+      p_description    => 'Provides access to the metadata catalog of the objects in the TICKETNEXUS schema.',
+      p_comments       => NULL); 
+
+  l_roles.DELETE;
+  l_modules.DELETE;
+  l_patterns.DELETE;
+
+  l_roles(1) := 'SODA Developer';
+  l_patterns(1) := '/soda/*';
+
+  ORDS.DEFINE_PRIVILEGE(
+      p_privilege_name => 'oracle.soda.privilege.developer',
+      p_roles          => l_roles,
+      p_patterns       => l_patterns,
+      p_modules        => l_modules,
+      p_label          => NULL,
+      p_description    => NULL,
+      p_comments       => NULL); 
+
+  l_roles.DELETE;
+  l_modules.DELETE;
+  l_patterns.DELETE;
+
+
+COMMIT;
+
+END;
+/
+
+
+-- sqlcl_snapshot {"hash":"cb4abe0ed5fe92b8bfac8eea507da3fcb0e80809","type":"ORDS_SCHEMA","name":"ords","schemaName":"TICKETNEXUS","sxml":""}
